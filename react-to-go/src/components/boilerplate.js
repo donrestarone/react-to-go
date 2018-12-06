2565b59c224164066ec4d9e985cf82226a87b2de
donrestarone <shashikejayatunge@gmail.com>
2018-12-06 09:40:43 -0500

    add components folder and boiler plate components in it, remove default create-react-app generated code

---
+++ react-to-go/src/components/boilerplate.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class MyClass extends Component {
+
+  render() {
+    return (
+    <div>
+
+    </div>
+    
+    );
+  }
+}
+
+export default MyClass;
---
+++ react-to-go/src/components/functionalBoilerplate.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const StatelessComponent = () => {
+  return <div></div>
+}
+
+export default StatelessComponent;
